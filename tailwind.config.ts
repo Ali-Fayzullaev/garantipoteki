024a146f12a7cfd5d366506f83b0dde44c598d69
Ali-Fayzullaev <alifayzullaev2020@gmail.com>
2025-10-01 21:37:50 +0300

    fixed error in the layout.tsx

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+export default {
+    darkMode: 'class',
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['var(--font-geist-sans)', 'system-ui', 'sans-serif'],
+          mono: ['var(--font-geist-mono)', 'ui-monospace', 'SFMono-Regular', 'monospace'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
